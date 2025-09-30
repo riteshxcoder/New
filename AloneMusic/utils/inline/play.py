d19c4f16d153fa6bc690a292717762c40ed6efc6
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-09-30 07:27:02 +0530

    Update play.py

--- AloneMusic/utils/inline/play.py
+++ AloneMusic/utils/inline/play.py
@@ -69,6 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [
+            InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"
+            )
+        ],
     ]
     return buttons
 
