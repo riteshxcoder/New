ade824c1d7f2f6e060d308ebf7193f8cc09acce7
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-10-02 10:23:50 +0530

    Update call.py

--- AloneMusic/core/call.py
+++ AloneMusic/core/call.py
@@ -18,7 +18,7 @@ from pytgcalls import PyTgCalls
 from pytgcalls.exceptions import NoActiveGroupCall
 from pytgcalls.types import Update
 #from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+#from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
